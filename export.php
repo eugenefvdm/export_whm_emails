f65c620408dd273484ebe6e60b14495c6d1a1584
Eugene van der Merwe <eugenevdm@gmail.com>
2023-05-09 20:45:42 +0200

    add a command line parameter to save output

--- export.php
+++ export.php
@@ -7,8 +7,14 @@ require_once "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-//echo print_r($_ENV['HOST'], true);
-//die();
+// Check if the required command line parameter is provided
+if ($argc != 2) {
+    echo "Usage: php export.php [filename]\n";
+    exit(1);
+}
+
+// Retrieve the command line parameter
+$filename = $argv[1];
 
 $api = new Cpanel([
     'host'      => $_ENV['WHM_HOST'],
@@ -17,27 +23,19 @@ $api = new Cpanel([
     'password'  => $_ENV['WHM_PASSWORD']
 ]);
 
-//echo print_r($api->listAccounts(), true);
-//die();
-
-//$accounts = json_decode($api->listAccounts());
-//die();
-
 $accounts = $api->listAccounts();
 
 foreach ($accounts['acct'] as $account) {
     $user = $account['user'];
-    getEmails($user);
-}
-
-function getEmails($user)
-{
-    global $api;
 
     $emails = $api->listEmailAccounts($user);
-    
+
+    $stream = fopen($filename, 'a');
+
     foreach ($emails['cpanelresult']['data'] as $data) {
         echo $user . "," . $data['email'] . "\n";
+        fwrite($stream, $user . "," . $data['email'] . "\n");
     }
-}
 
+    fclose($stream);
+}
